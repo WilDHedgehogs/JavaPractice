f7927f1548d8a8965861e15361aeaf491d863c17
Alexander <Flame911@mail.ru>
2020-07-12 17:51:19 +0300

    Задача №17: Запись данных в файл

---
+++ Practical Works/src/FileOutput.java
@@ -0,0 +1,44 @@
+import java.io.BufferedReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+//Задача №17: Запись данных в файл.
+public class FileOutput {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+        System.out.print("Enter file path and name: ");
+        String fileName = bufferedReader.readLine();
+
+        String line;
+        System.out.println("Enter 'stop' to end");
+        try (FileWriter fileWriter = new FileWriter(fileName)) {
+
+            while (true) {
+
+                System.out.print(": ");
+                line = bufferedReader.readLine();
+
+                if (line.equals("stop")) {
+
+                    break;
+
+                }
+
+                line += "\r\n";
+                fileWriter.write(line);
+
+            }
+
+        } catch (IOException e) {
+
+            e.printStackTrace();
+
+        }
+
+    }
+
+}
